e83440c07367a21eb0073975bbb8488b4772f85a
ciki1-hub <sonysliki@gmail.com>
2025-04-12 09:06:37 +0200

    Update start.sh

--- Streaming/start.sh
+++ Streaming/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Process environment variables into nginx config
-envsubst '${RTMP_AUTH_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${MOTOCROSS_API_SERVICE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start nginx
 exec nginx -g "daemon off;"
