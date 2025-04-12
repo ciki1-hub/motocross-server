708a15fab10ad095bcc11b560160b066ab709149
ciki1-hub <sonysliki@gmail.com>
2025-04-12 09:08:25 +0200

    Delete Streaming directory

--- Streaming/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Process environment variables into nginx config
-envsubst '${MOTOCROSS_API_SERVICE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start nginx
-exec nginx -g "daemon off;"
