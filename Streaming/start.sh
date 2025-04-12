305e93ee5423e52c6d2f903e21b1180c7f4c0e0d
ciki1-hub <sonysliki@gmail.com>
2025-04-12 08:58:29 +0200

    Update start.sh

--- Streaming/start.sh
+++ Streaming/start.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+# Process environment variables into nginx config
+envsubst '${MOTOCROSS_API_SERVICE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Start nginx
 exec nginx -g "daemon off;"
